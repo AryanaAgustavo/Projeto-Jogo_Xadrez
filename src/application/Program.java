759957bfeb88cdace7a69fe0c7884647120078a7
ArySilva <arysilva@rd.com.br>
2020-10-21 16:59:30 -0300

    implementação peças e tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
 import boardGame.Board;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
 
-        System.out.println(board.getColumns());
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
     }
 }
